00023260be17b33b3c5ba7feb0b09ee5b061335e
rjtmehta99 <rjtmehta99@gmail.com>
2017-08-15 00:41:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
 console.log('Loaded!');
+
+//Move the image when click
+var img = document.getElementById('pic');
+var marginLeft = 0;
+function moveRight()
+{
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
+img.onclick = function()
+{
+  var interval = setInterval(moveRight, 100);   
+};
