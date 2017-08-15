55bb5b347d173f9a0704475b94176b39cc6599e8
rjtmehta99 <rjtmehta99@gmail.com>
2017-08-15 16:00:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,7 @@
 console.log('Loaded!');
 
 //Move the image when click
+/*
 var img = document.getElementById('pic');
 var marginLeft = 0;
 function moveRight()
@@ -12,4 +13,20 @@ function moveRight()
 img.onclick = function()
 {
   var interval = setInterval(moveRight, 100);   
+}; 
+*/
+
+//Counter Code
+var button = document.getElementById('counter');
+button.onclick = function()
+{
+    //Make request to the counter end point
+    
+    //Capture the response and store it in the variable
+    
+    //Render the variable in the correct span
+    counter= counter+1;
+    var span= dpcument.getElementById('count');
+    span.innerHTML = counter.toString();
+    
 };
