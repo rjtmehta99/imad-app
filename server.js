31df76b04873b9aad823dc31801af7692adefb3b
rjtmehta99 <rjtmehta99@gmail.com>
2017-09-01 00:49:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
+
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 var articles=
 {
@@ -111,6 +115,29 @@ app.get('/hash/:input', function(req, res)
     res.send(hashedString);
 });
 
+app.get('/create-user', function(req,res)
+{
+   //username, password
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   
+   
+   var salt = crypto.getRandomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES($1, $2)',[username, dbString], function(err, result)
+   {
+      if(err)
+      {
+          res.status(500).send(err.toString());
+      }
+      
+      else 
+      {
+          res.send('User successfully created: ' + username);
+      }
+   });
+});
 
 var counter = 0;
 app.get('/counter', function(req,res)
